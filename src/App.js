aa79608aed6cfce188393d3099263fc5011444d2
thangaraj54321 <tr5479510@gmail.com>
2025-09-01 21:54:32 +0530

    Third

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import { useState } from "react";
 import Square from "./Square";
+import Input from "./Input";
 
 function App() {
    const [colorValues,setColorValues] =useState('')
    const [hexValues,setHexValues] = useState('')
    const [isDarkText,setIsDarkText] =useState(true)
-
-
-
-
-
-
   return (
   <div className="App">
-    <squre 
+    <Square 
      colorValues={colorValues}
      hexValues={hexValues}
      isDarkText={isDarkText}
     />
-    <input 
+     <Input
        colorValues={colorValues}
        setColorValues={setColorValues}
        setHexValues={setHexValues}
        isDarkText={isDarkText}
        setIsDarkText={setIsDarkText}
-    
-    />
+     />
 
   </div>
 
